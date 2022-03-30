755adb201936710cd34136a0777ac563dad19044
Tejas <jawaletejas1299@gmail.com>
2022-03-30 17:05:48 +0530

    Programs

---
+++ CopJAVA/PATTERN/src/PATTERN/Q16.java
@@ -0,0 +1,35 @@
+package PATTERN;
+
+public class Q16 {
+
+	public static void main(String[] args) {
+		int x;
+		int y;
+		int n = 5;
+
+		for (int i = 1; i <= n; i++) {
+			x = i;
+			y = n - i + 1;
+			for (int j = 1; j <= n; j++) {
+				if (j % 2 == 1) {
+					System.out.printf("%3d", x);
+				} else {
+					System.out.printf("%3d", y);
+				}
+				x += n;
+				y += n;
+
+			}
+			System.out.println();
+
+		}
+
+	}
+}
+
+
+//1 10 11 20 21
+//2  9 12 19 22
+//3  8 13 18 23
+//4  7 14 17 24
+//5  6 15 16 25
