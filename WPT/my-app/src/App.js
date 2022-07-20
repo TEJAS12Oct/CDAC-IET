b1eaa1aecbbc12f41cac1aee884fd87fdf8368a8
Tejas Satish Jawale <jawaletejas1299@gmail.com>
2022-07-20 09:11:25 +0530

    Upload Programs

---
+++ WPT/my-app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Welcome from './Welcome';
+import GoodMorning from './GoodMorning';
+import GoodBye from './GoodBye';
+
+
+
+function App() {
+  return (
+          <div>
+          <Welcome></Welcome>
+          <GoodMorning></GoodMorning>
+          <div>HI</div>
+           <div>Hello</div>
+           <GoodBye></GoodBye>
+           </div>
+          )
+     
+}
+
+export default App;
---
+++ WPT/my-app/src/GoodMorning.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+function GoodMorning()
+{
+    return (<div><p>Good Morning</p></div>)
+}
+
+export default GoodMorning
