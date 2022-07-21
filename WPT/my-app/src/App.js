cdfb03054f3cdb8f5742ef0cd0476cdab8148668
Tejas Satish Jawale <jawaletejas1299@gmail.com>
2022-07-21 09:50:41 +0530

    UPLOAD PROGRAMS

--- WPT/my-app/src/App.js
+++ WPT/my-app/src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import './App.css';
-import GoodMorning from './GoodMorning';
-import GoodBye from './GoodBye';
-import Factorial from './Factorial';
-import HelloC from './HelloC';
-import InputC from './InputC';
+// import GoodMorning from './GoodMorning';
+// import GoodBye from './GoodBye';
+// import Factorial from './Factorial';
+// import HelloC from './HelloC';
+// import InputC from './InputC';
 
 //Ting is an alias to normal export Greeting in ./Welcome
 //import {Greeting as Ting} from './Welcome';
 
 //Www is an alias to whatever is exported by default in ./Welcome file
 //import Www from './Welcome'
-import Www, {Greeting } from './Welcome'
+
+//import Www, {Greeting } from './Welcome'
+// import IsPrime from './IsPrime'
+// import Calc from './Calc'
+// import DoItTwice from'./DoItTwice'
 
 function App() 
 {
   return (
           <div>
-          <GoodMorning></GoodMorning>
+          {
+          /* <GoodMorning></GoodMorning>
           <div>HI</div>
           <div>Hello</div>
           <GoodBye></GoodBye>
@@ -29,8 +34,20 @@ function App()
           <InputC></InputC>
           <Factorial num= "10"></Factorial>
           <div className='SomeStyle'>HI</div>
-           <div>Hello</div>
-           </div>
+          
+          <div>Hello</div>
+          <center> QUESTION 1<div className="q1"><IsPrime num="7"></IsPrime></div>
+          <div className="q2"> QUESTION 2
+          <Calc  arr={[10,20]} oop='+'></Calc>
+          <Calc arr={[20,30]} oop='-'></Calc>
+          <Calc arr={[40,50]} oop='*'></Calc>
+          <Calc arr={[60,70]} oop='/'></Calc>
+          </div></center>
+          <DoItTwice  fun={()=>{return "THANK-YOU"}}></DoItTwice> */
+          }
+          
+          
+          </div>
           )
      
 }
