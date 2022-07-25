eed9613cf57341174ffa5e7641a565f66474f7a8
Tejas Satish Jawale <jawaletejas1299@gmail.com>
2022-07-25 16:46:11 +0530

    Remove

--- WPT/my-app/src/eq.js
+++
@@ -1,8 +0,0 @@
-let s="12"
-let x = 12
-
-//if(x==s) //checking the value --- doesnt bother about type
-if(x===s) // if typeof is different then it says different
-   console.log("same")
-else
-    console.log("different")
