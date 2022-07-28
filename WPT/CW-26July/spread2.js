d41b46242248aae2ac6cabf3b60da943850dbc7f
Tejas Satish Jawale <jawaletejas1299@gmail.com>
2022-07-28 07:21:12 +0530

    UPLOAD PROGRAMS

--- WPT/CW-26July/spread2.js
+++ WPT/CW-26July/spread2.js
@@ -1,18 +1,105 @@
-let obj={name:"asha",age:"22",country:"india",gender:"female"}
-let musician1 = {details : obj , stream : 'singer'}
-//let musician2 = {...musician1}
+/*Ex1 
+const arrValue = ['My', 'name', 'is', 'Jack'];
 
-// console.log("using musician 1 using print singer name : "+ musician1.details.name)
-// console.log("using musician 2 using print singer name : "+ musician2.details.name)
+console.log(arrValue);   // ["My", "name", "is", "Jack"]
+console.log(...arrValue);  
+//console.log(arrValue[0],arrValue[1],arrValue[2],arrValue[3])
+*/
+
+/*Ex2 
+const arr1 = ['one', 'two','six','seven'];
+const arr2 = [arr1, 'three', 'four', 'five'];
+const arr3 = [...arr1, 'three', 'four', 'five'];
+console.log(arr2, arr2.length)
+console.log(arr3,arr3.length)
+console.log(...arr2)
+*/
+
+// Ex3
+//let arr1 = [ 1, 2, 3];
+//let arr2 = arr1;  //shallow copy
+
+/*DEEP COPY 1
+let arr2 =[] 
+arr1.forEach((e)=>{ arr2.push(e)    })
+*/
+//DEEP COPY 2
+//let arr2 = arr1.map((e)=>{return e})
+
+/* DEEP COPY 3
+let arr2 = [...arr1]
+
+console.log(arr1); 
+console.log(arr2); 
+
+// append an item to the array
+arr1.push(4);
+
+console.log(arr1); 
+console.log(arr2); 
+ */
 
 
-let musician2 = Object.assign({}, musician1);
 
-musician1.details.name = "lata";
-musician1.details.age = 40;
+/*
+const obj1 = { x : 1, y : 2 };
+const obj2 = { z : 3 , x :25};
+//const obj3 ={x:obj1.x,y:obj1.y,z:obj2.z}
+// add members obj1 and obj2  to obj3
+//const obj3 = {...obj2,...obj1 };
+//const obj3 = {...obj1,...obj2 };
+//const obj3 ={obj1:obj1,obj2:obj2}  //same as  const obj3 = {obj1,obj2}
+console.log(obj3); 
+*/
 
-musician2.stream = "bollywood singers";
+/*
+const obj = { color:'red',name:'mars'}
+//let another = obj //one object two references
+let another = {...obj} //the properties are copied to the another address
 
-console.log("m1", musician1);
+another.name = 'jupiter'
+console.log(obj)   //output red mars
+console.log(another)  // red jupiter
+*/
 
-console.log("m2", musician2);
+/*
+let obj = {name:'lata mangeshar' , age:'92',country:'India',gender:'female'}
+let musician1 ={ details : obj, stream:'singer'}
+let musician2={...musician1}
+
+console.log("using musician 1 print singer name ",musician1.details.name)
+console.log("using musician 2 print singer name ",musician2.details.name)
+*/
+
+let obj = {name:'lata mangeshar' , age:'92',country:'India',gender:'female'}
+let musician1 ={ details : obj, stream:'singer'}
+
+//Assign is creating a deep copy
+let musician2 = Object.assign({},musician1)
+
+musician2.details.name = 'asha bhosle'
+musician2.details.age = 88
+
+//deep copy of obj is created and changed
+//musician2.details ={...musician2.details,name:'asha bhosle',age:88}
+musician2.stream = "hindi film singer"
+
+console.log("m1",musician1)
+console.log("m2",musician2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
