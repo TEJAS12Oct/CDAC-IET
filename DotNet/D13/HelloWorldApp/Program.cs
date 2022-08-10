980c766a66c43c9375e61781885c72589a4e7337
unknown <jawaletejas1299@gmail.com>
2022-08-10 09:15:31 +0530

    Upload Files

---
+++ DotNet/D13/HelloWorldApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int count=45;
+DateTime currentTime=DateTime.Now;
+Console.WriteLine("{0} {1} {2}", currentTime.Day, currentTime.Month, currentTime.Year);
+Console.WriteLine("count="+count);
+Console.WriteLine("Hello, World!");
---
+++ DotNet/D13/Onlineshopping/Controllers/ProductsController.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Onlineshopping.Models;
+
+namespace Onlineshopping.Controllers;
+
+public class ProductsController : Controller
+{
+    private readonly ILogger<ProductsController> _logger;
+
+    public ProductsController(ILogger<ProductsController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Details(int id)
+    {
+
+        switch(id){
+            case 1:
+            {ViewData["product"]=new  Product{ Id=1, 
+                                           Title="Gerbera", 
+                                           Description="Wedding Flower",
+                                           Quantity=6700,
+                                          };
+
+            }
+            break;
+
+            case 2:
+            {
+                ViewData["product"]=new  Product{ Id=1, 
+                                           Title="Rose", 
+                                           Description="Valentine Flower",
+                                           Quantity=600,
+                                          };
+            }
+            break;
+            case 3:
+            {
+                ViewData["product"]=new  Product{ Id=1, 
+                                           Title="Marigold", 
+                                           Description="Festival Flower",
+                                           Quantity=8700,
+                                          };
+            }
+            break;
+        }
+        
+        return View();
+    }
+
+       
+
+}
