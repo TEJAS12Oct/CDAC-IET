0cde3cdf89ee58eb28857475556d6432b72b4560
unknown <jawaletejas1299@gmail.com>
2022-08-20 21:45:30 +0530

    Upload Programs

---
+++ EXAM/WPT/src/Comp2.js
@@ -0,0 +1,10 @@
+import React from "react"
+import './App.css';
+
+function Comp2() {
+  return (
+   <h1>Hiiii</h1>
+  );
+}
+
+export default Comp2;
---
+++ EXAM/WPT/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+import Exam from './Exam' 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Exam/>);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
